8ef79e86b7fcba96446ac84412ee529faa3b19ce
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-09-11 11:16:47 -0500

    Login View

--- Src/Js/Controllers/IndexController.js
+++ Src/Js/Controllers/IndexController.js
@@ -1,15 +1,38 @@
-angular.module('App', []).controller('HeaderController', ($scope) => {
-    const { remote } = require('electron');
+angular.module('App', ['ngRoute', 'Usuarios'])
+    .controller('HeaderController', function ($scope) {
+        const { remote, ipcRenderer } = require('electron');
 
-    $scope.Minimizar = () => {
-        remote.getCurrentWindow().minimize();
-    }
+        $scope.Minimizar = function () {
+            remote.getCurrentWindow().minimize();
+        }
 
-    $scope.Maximizar = () => {
-        remote.getCurrentWindow().isMaximized() ? remote.getCurrentWindow().unmaximize() : remote.getCurrentWindow().maximize();
-    }
+        $scope.Maximizar = function () {
+            remote.getCurrentWindow().isMaximized() ? remote.getCurrentWindow().unmaximize() : remote.getCurrentWindow().maximize();
+        }
 
-    $scope.Cerrar = () => {
-        remote.app.exit();
-    }
-})
+        $scope.Cerrar = function () {
+            remote.app.exit();
+        }
+
+        $scope.Mover = function (ev) {
+            ipcRenderer.send({
+                x: ev.x,
+                y: ev.y
+            })
+        }
+    })
+    .config(function ($routeProvider, $locationProvider) {
+
+        $routeProvider.when('/Login', {
+            templateUrl: '../Pages/Login.html',
+            controller: 'Usuarios'
+        })
+            .otherwise({
+                redirectTo: '/Login'
+            });
+
+        $locationProvider.html5Mode({
+            enable: true,
+            requireBase: false
+        });
+    });
---
+++ Src/Js/Controllers/Usuarios.js
@@ -0,0 +1,3 @@
+angular.module('Usuarios', []).controller('Usuarios', function ($scope) {
+
+})
