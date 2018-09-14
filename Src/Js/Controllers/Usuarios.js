976feecb8de1485e6a074d2a3696b4ae638238eb
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-09-13 21:15:06 -0500

    Form Validacion

--- Src/Js/Controllers/Usuarios.js
+++ Src/Js/Controllers/Usuarios.js
@@ -1,3 +1,30 @@
 angular.module('Usuarios', []).controller('Usuarios', function ($scope) {
 
+    $('#Registro')
+        .form({
+            fields: {
+                Nombre: 'empty',
+                Email: 'empty',
+                Password: ['minLength[6]', 'empty'],
+            }
+        });
+
+    $('#Login')
+        .form({
+            fields: {
+                Email: 'empty',
+                Password: ['minLength[6]', 'empty'],
+            }
+        });
+
+    $scope.Usuario = $('#Registro').form('get values');
+    $scope.UsuarioLogin = $('#Login').form('get values');
+
+    $scope.Registrar = function () {
+        console.log($scope.Usuario);
+    }
+
+    $scope.Login = function () {
+        console.log($scope.UsuarioLogin);
+    }
 })
